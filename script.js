41686cb948b5be14bbc458a8e5cf80063e87e061
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-22 14:57:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let body=document.querySelector("body");
+let p=document.createElement("p");
+function updateTime() {
+	p.setAttribute("id","timer")
+    let dt= new Date();
+    p.textContent = dt.toLocaleString();
+}
+setInterval(updateTime,1000)
+body.appendChild(p);
+
+
